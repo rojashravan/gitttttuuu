d0b20cc2a83b912499a5b9867f442b933b1d006d
rojashravan <deekondaroja04@gmail.com>
2023-06-06 15:53:24 +0530

    github technologies

---
+++ src/com/slokam/cleareclipse.java
@@ -0,0 +1,13 @@
+package com.slokam;
+
+public class cleareclipse {
+      String name="roja";
+      
+	public static void main(String[] args) {
+		
+		cleareclipse c= new cleareclipse();
+		System.out.println("name");
+
+	}
+
+}
